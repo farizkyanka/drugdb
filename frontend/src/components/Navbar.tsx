e023046312d009f2e51607b27b8c9f2443ce064f
Anka <farizkyanka@outlook.com>
2024-06-11 21:25:00 +0700

    navbar enhancements

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,9 +1,16 @@
-import { Link } from "react-router-dom";
 import { User } from "../contexts/UserContext";
 import SearchBar from "./SearchBar";
+import { HomeLink, NewDrugLink, LogoutLink } from "./Links";
+import { useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoMdClose } from "react-icons/io";
 
 export default function Navbar() {
   const isLoggedIn = User().isLoggedIn;
+
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+
+  const handleNavbarClick = (e: any) => setIsNavbarOpen(!isNavbarOpen);
 
   return (
     <>
@@ -12,38 +19,47 @@ export default function Navbar() {
           className="container flex justify-around max-w-screen-lg mx-auto md:px-4"
           aria-label="Global"
         >
-          <div className="hidden md:flex flex-row w-1/4">
-            <Link
-              to=""
-              className="mx-2 px-2 flex flex-col justify-center text-white hover:bg-cyan-600"
-            >
-              <h6>Home</h6>
-            </Link>
-            {isLoggedIn && (
-              <Link
-                to="drugdb/admin/new-drug"
-                className="mx-2 px-2 flex text-white flex-col justify-center hover:bg-cyan-600"
-              >
-                <h6>Add new drug</h6>
-              </Link>
-            )}
+          <div className="hidden md:flex">
+            <HomeLink />
           </div>
-
+          {isLoggedIn ? (
+            <div
+              className="m-2 w-16 flex md:hidden justify-center items-center rounded-md text-slate-200 bg-slate-600"
+              onClick={handleNavbarClick}
+            >
+              {isNavbarOpen ? <IoMdClose /> : <RxHamburgerMenu />}
+            </div>
+          ) : (
+            <div className="m-2 md:hidden rounded bg-slate-600 flex">
+              <HomeLink />
+            </div>
+          )}
+          <div className="hidden md:flex flex-row w-1/4">
+            {isLoggedIn && <NewDrugLink />}
+          </div>
           <div className="flex grow m-2">
             <SearchBar />
           </div>
-          <div className="flex flex-row-reverse w-1/4">
-            {isLoggedIn && (
-              <Link
-                to="./admin/logout"
-                className="mx-2 px-2 flex flex-col justify-center text-white hover:bg-cyan-600"
-              >
-                <h6>Logout</h6>
-              </Link>
-            )}
+          <div className="hidden md:flex flex-row-reverse w-1/4">
+            {isLoggedIn && <LogoutLink />}
           </div>
         </nav>
       </header>
+      <ul
+        className={`${
+          isNavbarOpen ? "" : "hidden"
+        } w-full absolute bg-slate-800 z-10`}
+      >
+        <li className="my-4" onClick={handleNavbarClick}>
+          <HomeLink />
+        </li>
+        <li className="my-4" onClick={handleNavbarClick}>
+          <NewDrugLink />
+        </li>
+        <li className="my-4" onClick={handleNavbarClick}>
+          <LogoutLink />
+        </li>
+      </ul>
     </>
   );
 }
